9f67565484dcaf34dfea81cb8944fa1baf1fa633
RomanKamko <r.kamko@mail.ru>
2023-03-07 10:40:19 +0400

    initial commit

---
+++ src/edu/skypro/homework/Bicycle.java
@@ -0,0 +1,10 @@
+package edu.skypro.homework;
+
+public class Bicycle {
+    public String modelName;
+    public int wheelsCount;
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
